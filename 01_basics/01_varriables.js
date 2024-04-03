835827e03114c5e685313ebce69e71f1697172c0
Gagandeep Singh <121658538+GagandeepSingh521@users.noreply.github.com>
2024-04-03 17:48:49 +0000

    how to declare varriables

---
+++ 01_basics/01_varriables.js
@@ -0,0 +1,12 @@
+const accountId=144523
+let accountEmail="Gagan@gmail.com"
+var accountPassword="12345"
+accountCity="Mohali"
+
+// accountId=2   //not allowed to change the constant varriable
+
+accountEmail="daljit@gmail.com"
+accountPassword="54321"
+accountCity="Chandigarh"
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
