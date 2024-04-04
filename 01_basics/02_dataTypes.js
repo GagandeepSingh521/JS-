1b3c58e7c23cf4b7dcf99a91d52fd17f3380f39e
Gagandeep Singh <121658538+GagandeepSingh521@users.noreply.github.com>
2024-04-04 02:09:27 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict" // treat all java script code as newer version
+
+// alert ("Gagan")  // we are using Node.js as environment not the browswe v8 engine as compiler
+
+console.log(3+              //not good , code readbility should be on high priority 
+    3)
+
+// 1. Primtive data types
+    //number  => range up 2 the power 53,
+    // bigInt => usually for stock market related projects
+    //string,
+    //boolean   =>true/false
+    //undefined     => like let name;   // varriable declare but not assign any value yet,
+    
+    // null     => its a standalone value  like let temprature=null, // means value not fetch            
+                   //yet, it doesn't mean the value is empty. empty value is different
+
+    //symbol   => used for unique ness
+
+
+    // 2. Non-primtive data types
+
+    //  object
+
+    console.log(typeof undefined)  // its undefined
+
+    console.log(typeof null)        // its give us object, some times says it the drawback of the language
+
