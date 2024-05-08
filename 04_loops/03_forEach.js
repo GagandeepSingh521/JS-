86e736dfa01e0eb277385692701756da97bbba8b
Gagandeep Singh <121658538+GagandeepSingh521@users.noreply.github.com>
2024-05-08 17:29:33 +0000

    flters and for each

--- 04_loops/03_forEach.js
+++ 04_loops/03_forEach.js
@@ -1,10 +1,11 @@
 // it takes a call back function , call back fn has no any name
+//forEach doesnt return any value , it just checking or updating the existing array
 
 const array=["js","php","java","c++"]
 
 array.forEach(element => {
 
-    console.log(element);
+    // console.log(element);
 });
 
 /** output
@@ -17,7 +18,7 @@ c++
 // it also have the full array in parameters
 
 array.forEach((element,index,array) => {
-    console.log(element, index, array);
+    // console.log(element, index, array);
 });
 
 /**
@@ -26,3 +27,34 @@ php 1 [ 'js', 'php', 'java', 'c++' ]
 java 2 [ 'js', 'php', 'java', 'c++' ]
 c++ 3 [ 'js', 'php', 'java', 'c++' ]
  */
+
+// forEach method doesnt return any value
+
+const values=array.forEach((element)=> {return element})
+
+// console.log("value is-->",values)
+
+/**value is--> undefined */
+
+//to return values filter is used
+
+const arr=[1,2,3,4,5,6,7,8,9,10]
+
+const newArray=arr.filter((item)=>item>5)   //it return the values which satisfy the condition
+
+console.log("new array is-->",newArray)
+
+//Output is:- new array is--> [ 6, 7, 8, 9, 10 ]
+
+//same work we can do with forEach but in below menstioned way
+
+let anotherArray=[]
+
+arr.forEach((num)=>
+    {
+        if(num>5){
+            anotherArray.push(num)
+        }
+        })
+
+        console.log("another array-->",anotherArray)
